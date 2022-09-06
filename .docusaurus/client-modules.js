cc2612109f036a49949dbb1d865a8790cf9f9a50
theprashantchandra <97657867+theprashantchandra@users.noreply.github.com>
2022-09-06 12:21:34 +0530

    wellmed OGA

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('D:\\MyProjects\\Project OGA\\5 Sept 2022 1 Y Profile with proper Alignment\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\MyProjects\\Project OGA\\5 Sept 2022 1 Y Profile with proper Alignment\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\MyProjects\\Project OGA\\5 Sept 2022 1 Y Profile with proper Alignment\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('D:\\MyProjects\\Project OGA\\5 Sept 2022 1 Y Profile with proper Alignment\\src\\css\\custom.css'),
+];
